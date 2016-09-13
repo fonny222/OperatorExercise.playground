ac6b92dfb7f3e6bf92057b2321a4479d623edda4
fonny222 <cfontana@e112-m-18.mchenry.edu>
2016-09-13 17:24:52 -0500

    Calculator exercise
    calculator exercise first swift class.

---
+++ Contents.swift
@@ -0,0 +1,45 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+// initialize variables 
+//**********SECTION ONE******************
+
+
+var a = 5;
+var b = 3;
+var c = 35;
+var d = 32;
+
+
+var a1 = 0;
+var b1 = 0;
+var c1 = 0;
+var d1 = 0;
+var P = 0;
+
+
+// calculations
+//***********SECTION TWO***************
+
+a1 = a + b;
+b1 = b - a;
+c1 = c * a;
+d1 = d / a;
+P = d % b;
+
+
+
+//Print to the Console
+//*************SECTION THREE*************
+
+print("\(a) + \(b) = \(a1)")
+print("\(b) - \(a) = \(b1)")
+print("\(c) * \(a) = \(c1)")
+print("\(d) / \(a) = \(d1)")
+print("\(d) % \(a) = \(P)")
+
+
+
+
